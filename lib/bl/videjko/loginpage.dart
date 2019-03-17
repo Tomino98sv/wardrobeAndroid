a7867a6e796b882d54d08a150f0eb88866781db2
Tomino98sv <tomas.pavlik@akademiasovy.sk>
2019-03-17 12:32:16 +0100

    validation on inputs for sign in and sign up

--- lib/bl/videjko/loginpage.dart
+++ lib/bl/videjko/loginpage.dart
@@ -20,33 +20,39 @@ class LoginPage extends StatefulWidget {
   String _email;
   String _password;
   FirebaseUser user;
-
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: Container(
+      body: Form(
+        key: _formKey,
+      child: Container(
         padding: EdgeInsets.all(25.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextField(
+            TextFormField(
               decoration: InputDecoration(hintText: 'Email'),
-              onChanged: (value) {
-                setState(() {
-                  _email = value;
-                });
+              validator: (input){
+                if(input.isEmpty){
+                  return 'Please type an email';
+                }else if(validateEmail(input)){
+                  return 'Mail must be in mail format (%@%.%)';
+                }
               },
+              onSaved: (input) => _email = input,
             ),SizedBox(height: 15.0),
-            TextField(
+            TextFormField(
               decoration: InputDecoration(hintText: 'Password'),
-              onChanged: (value){
-                setState(() {
-                  _password = value;
-                });
+              validator: (input){
+                if(input.length < 6){
+                  return 'Your password needs to be at least 6 characters';
+                }
               },
+              onSaved: (input) => _password = input,
               obscureText: true,
             ),
             SizedBox(height: 20.0),
@@ -55,18 +61,7 @@ class LoginPage extends StatefulWidget {
               color: Colors.blue,
               textColor: Colors.white,
               elevation: 7.0,
-              onPressed: (){
-                FirebaseAuth.instance
-                    .signInWithEmailAndPassword(
-                    email: _email, password: _password)
-                    .then((FirebaseUser user){
-                      Navigator.of(context).pushReplacementNamed('/homepage');
-                    })
-                    .catchError((e){
-                      print("NO LOGGING");
-                      print(e);
-                });
-              },
+              onPressed: signInMethod,
             ),
             SizedBox(height: 15.0),
             Text('Don\'t have an account?'),
@@ -83,7 +78,40 @@ class LoginPage extends StatefulWidget {
           ],
         ),
       ),
+    )
     );
   }
+
+
+  void signInMethod() async {
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+          email: _email, password: _password)
+          .then((FirebaseUser user){
+        Navigator.of(context).pushReplacementNamed('/homepage');
+      })
+          .catchError((e){
+        print("NO LOGGING validation was passed but not loggin to firebaseAuth");
+        print(e);
+      });
+    }else{
+      debugPrint("validation of sign in to firebaseAuth not pass");
+    }
+  }
+
+
+  bool validateEmail(String value) {
+    Pattern pattern =r'^[a-zA-Z0-9\.\-\_]+\@[a-zA-Z0-9]+\.[a-zA-Z0-9]{2,}$';
+    RegExp regex = new RegExp(pattern);
+    if(!regex.hasMatch(value)){
+      return true;
+    }else{
+      return false;
+    }
+
+  }
+
 }
 
