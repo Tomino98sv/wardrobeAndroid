a674099c181a2c978b3b6967d6b7204c6fc997d2
MiriamVar <mimika648@gmail.com>
2019-03-19 00:28:08 +0100

    changed design of floating button

--- lib/db/model/Item.dart
+++ lib/db/model/Item.dart
@@ -3,7 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/st/storage/stPage.dart';
 
-void main() => runApp(MyNewItem());
+//void main() => runApp(MyNewItem());
 
 class MyNewItem extends StatefulWidget {
   @override
