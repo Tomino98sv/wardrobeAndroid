528e7e6041ae1cc1fc342c535aed5d6e97cf84d3
Karabasova <43435796+Karabasova@users.noreply.github.com>
2019-05-10 21:43:58 +0200

    create account

--- index.js
+++ index.js
@@ -9,12 +9,20 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome user: " + email_id;
+      var email_verified = user.emailVerified;
+
+      if(email_verified){
+        document.getElementById("verify_btn").style.display = "none";
+      } else {
+        document.getElementById("verify_btn").style.display = "block";
+      }
+
+      document.getElementById("user_para").innerHTML = "Welcome user: " + email_id + "<br/> Verified: " + email_verified;
 
     }
 
   } else {
-    
+
     document.getElementById("user_div").style.display = "none";
     document.getElementById("login_div").style.display = "block";
 
@@ -27,7 +35,21 @@ function login(){
   var userPass = document.getElementById("password_field").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
-   
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+  });
+
+}
+
+function create_account(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
 
@@ -39,4 +61,13 @@ function login(){
 
 function logout(){
   firebase.auth().signOut();
+}
+
+function send_verification(){
+  var user = firebase.auth().currentUser;
+  user.sendEmailVerification().then(function(){
+    window.alert("Verification was sent.");
+  }).catch(function(error){
+    window.alert("Error: " + error.message);
+  });
 }
