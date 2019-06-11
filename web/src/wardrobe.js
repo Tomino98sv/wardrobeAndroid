61e8b7825f5549233fc7e194511bf2ccf2e3c90f
Karabasova <43435796+Karabasova@users.noreply.github.com>
2019-06-11 13:20:40 +0200

    price

--- web/src/wardrobe.js
+++ web/src/wardrobe.js
@@ -125,7 +125,7 @@ function updateCartTotal() {
         total = total + (price * quantity)
     }
     total = Math.round(total * 100) / 100
-    document.getElementsByClassName('cart-total-price')[0].innerText = total + ' eur'
+    document.getElementsByClassName('cart-total-price')[0].innerText = '0 eur'
 }
 
 function getDownloadURL() {
